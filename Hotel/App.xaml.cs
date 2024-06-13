f5c137f8551a197cb6329fda2278b5899bcde829
Vitaly Zhukov <ikiruhawk@gmail.com>
2024-06-13 15:58:17 +0400

    Add project files.

---
+++ Hotel/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+
+namespace Hotel;
+public partial class App : Application
+{
+}
---
+++ Hotel/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Hotel;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
